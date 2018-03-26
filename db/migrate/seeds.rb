aab69f605615648a61ef2b2369f5d07dd994321c
github username <github email address>
2018-03-26 18:09:17 -0400

    We added player names to the database from the API. Note trouble with seed file. Note to self dont forget to call the method name. Don't put seeds inside migrate file.

--- db/migrate/seeds.rb
+++
@@ -1,9 +0,0 @@
-require 'rest-client'
-require 'JSON'
-
-def get_player_name
-  player_data = RestClient.get("http://data.nba.net/10s/prod/v1/2016/players.json")
-  player_hash = JSON.parse(player_data)
-
-  player_hash.each do |
-end
---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+require 'rest-client'
+require 'JSON'
+require 'pry'
+
+def get_player_name
+  player_data = RestClient.get("http://data.nba.net/10s/prod/v1/2016/players.json")
+  player_hash = JSON.parse(player_data)
+
+  player_hash.each do |key, value|
+    if key == "league"
+
+      value.each do |standard, player_names_array|
+        player_names_array.each do |player_info|
+
+          Player.create(first_name: player_info['firstName'], last_name: player_info['lastName'])
+
+        end
+      end
+    end
+
+    puts "hi"
+  end
+end
+
+get_player_name
+Player.all
