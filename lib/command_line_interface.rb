c322fd5fae1ee4408b217f1ec870dd35fc481098
github username <github email address>
2018-03-27 15:03:17 -0400

    1. added input_choice 1 and 2 options for entering name or position
    2. created new method input_player_position
    
    3. created find_player_by_position method
    
    4. created find_player_name_by_position
    
    5. added conditional if statements for new user input to find either by
    name or position

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -9,9 +9,20 @@ class CommandLineInterface
     User.create(name: input)
   end
 
-  def input_player_name
+  def input_choice
     puts 'To begin your draft, search for desired players.'
-    puts 'To search a player by name, enter name.'
+    puts "to search a player by name enter 1"
+    puts 'To search a player by position, enter 2'
+    gets.chomp
+  end
+
+  def input_player_name
+    puts 'Enter player name.'
+    gets.chomp
+  end
+
+  def input_player_position
+    puts 'Enter player position.'
     gets.chomp
   end
 
@@ -19,22 +30,46 @@ class CommandLineInterface
     Player.find_by(full_name: player_name)
   end
 
+  def find_player_by_position(player_posiiton)
+     Player.where("position == '#{player_posiiton}'")
+  end
+
+  def find_player_name_by_position(player_position_array, player_name)
+    player_position_array.find_by(full_name: player_name)
+
+  end
+
   def ask_user_add(player_name)
     p "Add #{player_name.full_name} to your roster? Enter (Y/N)."
     gets.chomp
   end
-  
+
 end
+
 
 
 def run
   new_cli = CommandLineInterface.new
   username = new_cli.greet
   new_user = new_cli.create_username(username)
-  input = new_cli.input_player_name
-  player_name = new_cli.find_player_by_name(input)
-  input2 = new_cli.ask_user_add(player_name)
-  if input2 == 'Y'
-    new_user.add_player_to_roster(player_name)
-  end
+  input = new_cli.input_choice
+    if input == "1"
+      nba_player = new_cli.input_player_name
+      player_name = new_cli.find_player_by_name(nba_player)
+      input2 = new_cli.ask_user_add(player_name)
+      if input2 == 'Y'
+        new_user.add_player_to_roster(player_name)
+      end
+    elsif input == "2"
+      nba_posiiton = new_cli.input_player_position
+      players_position = new_cli.find_player_by_position(nba_posiiton)
+      input3 = new_cli.input_player_name
+      player_name = find_player_name_by_position(players_position, input3)
+      input4 = new_cli.ask_user_add(player_name)
+        if input4 == "Y"
+          new_user.add_player_to_roster(player_name)
+        end
+    end
+
+
 end
