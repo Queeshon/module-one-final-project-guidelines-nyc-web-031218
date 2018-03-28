4bf385fbb021fd8c2c50efe1f673f328aca235fe
github username <github email address>
2018-03-28 14:58:51 -0400

    1. created a counter for user_roster 2. need to create actual user roster

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CommandLineInterface
 
   def greet
@@ -7,16 +9,17 @@ class CommandLineInterface
 
   def create_username(input)
     User.create(name: input)
+    #puts 'To begin your draft, search for desired players.'.bold.yellow
   end
 
   def input_choice
-    puts 'To begin your draft, search for desired players.'.bold.yellow
-    sleep(2)
     puts "To search a player by name, enter 1.".bold.yellow
     sleep(1)
     puts 'To search players by position, enter 2.'.bold.yellow
     sleep(1)
     puts "To search players by team, enter 3.".bold.yellow
+    sleep(1)
+    puts "To exit the NBA Fantasy Mock Draft application, enter 4.".bold.red
     gets.chomp
   end
 
@@ -60,7 +63,13 @@ class CommandLineInterface
     gets.chomp
   end
 
+  def neglected_player
+    p ""
+  end
+
 end
+
+
 
 
 
@@ -68,14 +77,29 @@ def run
   new_cli = CommandLineInterface.new
   username = new_cli.greet
   new_user = new_cli.create_username(username)
-  input = new_cli.input_choice
-    if input == "1"
+  user_roster = 0
+  #binding.pry
+
+  loop do
+    input = new_cli.input_choice
+    if user_roster > 4
+      p "Roster full. View roster?"
+      p new_user.roster
+      break
+    #this is the method to call a player by name.
+    elsif input == "1"
       nba_player = new_cli.input_player_name
       player_name = new_cli.find_player_by_name(nba_player)
       input2 = new_cli.ask_user_add(player_name)
-      if input2 == 'Y'
+      if input2 == 'Y' || input2 == 'y'
         new_user.add_player_to_roster(player_name)
+        user_roster +=1
+      elsif input2 == 'N' || input2 == 'n'
+        redo
+      else
+        p "Invalid response. Please enter Y/N.".bold.red
       end
+    #this is the method to call a player by position
     elsif input == "2"
       nba_position = new_cli.input_player_position
       players_position = new_cli.find_player_by_position(nba_position)
@@ -83,9 +107,15 @@ def run
       input3 = new_cli.input_player_name
       player_name = new_cli.find_player_name_by_position(players_position, input3)
       input4 = new_cli.ask_user_add(player_name)
-        if input4 == "Y"
+        if input4 == 'Y' || input4 == 'y'
           new_user.add_player_to_roster(player_name)
-        end
+          user_roster +=1
+        elsif input4 == 'N' || input4 == 'n'
+          redo
+        else
+          p "Invalid response. Please enter Y/N.".bold.red
+      end
+    #this is the method to call a player by team
     elsif input == "3"
       nba_team = new_cli.input_player_team
       roster = new_cli.find_player_by_team(nba_team)
@@ -93,10 +123,20 @@ def run
       input5 = new_cli.input_player_name
       player_name = new_cli.find_player_name_by_team(roster, input5)
       input6 = new_cli.ask_user_add(player_name)
-        if input6 == "Y"
+        if input6 == 'Y' || input6 == 'y'
           new_user.add_player_to_roster(player_name)
+          user_roster +=1
+        elsif input6 == 'N' || input6 == 'n'
+          redo
+        else
+          p "Invalid response. Please enter Y/N.".bold.red
         end
+    #this is the method to break the loop
+    elsif input == "4"
+      p "K"
+      break
+    else
+      puts "Invalid response. Please enter 1, 2, or 3.".bold.red
     end
-
-
+  end
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   has_many :drafts
   has_many :players, through: :drafts
 
+  attr_accessor :roster
+
   def add_player_to_roster(player)
     Draft.create(player_id: player.id, user_id: self.id)
+    roster << player.full_name
   end
 
 end
